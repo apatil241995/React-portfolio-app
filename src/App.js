530f3165d32e14a890e14f9c62db31dd8c328830
apatil241995 <patilanish39@gmail.com>
2022-04-12 18:07:24 +0530

    Created Portfolio page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import m_images from './images/1.jpg'
 
-function App() {
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main-div">
+      <header className="header">
+        <div className="samp">
+          Anish Patil
+          <br></br>
+          <h>Web Devloper</h>
+        </div>
+        <samp className="space"> ------ </samp>
+        <div className="samp">
+          <a className="anchor">About me</a>
+        </div>
+        <samp className="space"> ------ </samp>
+        <div className="samp">Contact</div>
       </header>
     </div>
   );
 }
 
-export default App;
+function Mainimage() {
+  return (
+    <div>
+      <img className='m_image' src={m_images} alt=' '></img>
+    </div>
+  );
+}
+
+function Background() {
+}
+
+function Introtext() {
+  return (
+    <div className='introText'>
+      <h3>Hi i am Anish.</h3>
+      <h1>Web Devloper at Mindbowser.</h1>
+      <h2>Frindly Nature Loves to meet new pepole.</h2>
+      <h4>More</h4>
+    </div>
+  );
+}
+
+function Personalinfo() {
+  return (
+    <div className="personal_info">
+      <h1>Hi, I am Anish Vijay Patil</h1>
+      <h3>
+        <p >
+          I am basicaly from Alibag Currently leaving in Pune.I have completed my bachlores from Pune University.I have completed my masters from Dr. BATU University, Lonere. Currently i am working at Mindbowser As trainee web
+          Devloper.
+        </p>
+      </h3>
+    </div>
+  );
+}
+
+function Workandhobbies() {
+  return (
+    <div className="work-hobbies">
+      <h1>Work and Hobbies</h1>
+      <h3>
+        <p>
+          I have 2 years working experieance in electrical feild as a Line Head in production department. For last one year i Started learing we devlopment, Currently i am working as a trainee web devloper in Mindbowser. My Hobbies
+          are learing new technologies, playing indoor and outdoor games, playing vertual gemes and travling.
+        </p>
+      </h3>
+    </div>
+  );
+}
+
+export default Navbar;
+export { Mainimage };
+export { Background };
+export { Introtext };
+export { Personalinfo };
+export { Workandhobbies };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Personalinfo } from './App';
+import Navbar from './App';
+import { Background } from './App';
+import { Mainimage } from './App';
+import { Introtext } from './App';
+import { Workandhobbies } from './App';
 
-ReactDOM.render(
+const navbarContainer = document.getElementById('NavBar');
+const navbarRoot = createRoot(navbarContainer);
+navbarRoot.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
-  document.getElementById('root')
+);
+
+const mainimageContainer = document.getElementById('MainImage');
+const mainimageRoot = createRoot(mainimageContainer);
+mainimageRoot.render(
+  <React.StrictMode>
+    <Mainimage />
+    <Background />
+  </React.StrictMode>,
+);
+
+const introtextContainer = document.getElementById('IntroText');
+const introtextRoot = createRoot(introtextContainer);
+introtextRoot.render(
+  <React.StrictMode>
+    <Introtext />
+  </React.StrictMode>,
+);
+
+const personalinfoContainer = document.getElementById('PersonalInfo');
+const personalinfoRoot = createRoot(personalinfoContainer);
+personalinfoRoot.render(
+  <React.StrictMode>
+    <Personalinfo />
+  </React.StrictMode>,
+);
+
+const workandhobbiesContainer = document.getElementById('WorkandHobbies');
+const workandhobbiesRoot = createRoot(workandhobbiesContainer);
+workandhobbiesRoot.render(
+  <React.StrictMode>
+    <Workandhobbies />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
